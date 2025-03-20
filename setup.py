75347f76acf99a33fc5f23b72cd6c788921112df
Neranjana1408 <ss.neranjana@gmail.com>
2025-03-20 16:25:26 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'CuraMateDR',
+    version = '0.0.0',
+    author = 'Neranjana',
+    author_email = 'ss.neranjana@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+)
